58af1dd16bdd01fe17f091920e04d1ece1fc9a07
chinmaykd21 <chinmaykd21@gmail.com>
2024-05-26 21:51:46 -0400

    fix production error in dynamic routing

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,10 @@
 import CardWrapper from "@/components/card-wrapper";
 import { getProduct, type Product } from "@/lib/data";
 
-const Product = async ({ searchParams }: { searchParams: { id: string } }) => {
-  if (!searchParams?.id) return null;
+const Product = async ({ params }: { params: { id: string } }) => {
+  if (!params?.id) return null;
 
-  const { data: product, error } = await getProduct(searchParams.id);
+  const { data: product, error } = await getProduct(params.id);
 
   if (error || !product) return null;
 
