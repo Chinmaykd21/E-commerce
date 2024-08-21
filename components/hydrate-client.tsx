a8ce9e16b5c358c5074509456fd43722ea1e7b2e
chinmaykd21 <chinmaykd21@gmail.com>
2024-08-20 22:33:48 -0400

    fix hydration reset upon refresh

--- components/hydrate-client.tsx
+++ components/hydrate-client.tsx
@@ -5,6 +5,9 @@ import { useEffect } from "react";
 
 export const HydrateClient = () => {
   useEffect(() => {
+    // Hydrate the Zustand store with data from localStorage when the component mounts
+    useCartStore.getState().hydrateCart();
+
     const handleStorageUpdate = (event: StorageEvent) => {
       if (event.key === "cart") {
         const updatedCart = event.newValue ? JSON.parse(event.newValue) : [];
