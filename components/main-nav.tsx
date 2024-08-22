6ed62783de623bdea8e0e93e00869a97953aa44f
chinmaykd21 <chinmaykd21@gmail.com>
2024-08-22 17:42:59 -0400

    add some styles

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,12 +5,12 @@ import { SearchProduct } from "./search-product-simpler";
 
 const MainNav = () => {
   return (
-    <section className="flex flex-col-reverse p-8 mb-5 gap-3 justify-between items-center space-x-2">
+    <section className="flex flex-col-reverse justify-between items-center sm:flex-row p-8 mb-5 gap-3 space-x-2">
       {/* TODO: Fallback component here */}
       <Suspense>
         <SearchProduct />
       </Suspense>
-      <div className="flex justify-between items-center space-x-3">
+      <div className="flex w-full justify-between items-center space-x-3 sm:w-1/12">
         <ShoppingCartButton />
         <ToggleTheme />
       </div>
